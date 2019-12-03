228e5b9be3392011262a8c113090b5081e917e92
Gonewiththewind4 <523443113@qq.com>
2019-12-03 10:20:07 +0800

    Update fengzhuanga.m

--- mentoCarlo50/fengzhuanga.m
+++ mentoCarlo50/fengzhuanga.m
@@ -1,24 +1,24 @@
 
-[as(1),asd(1),asd2(1)]=finaltextfirsta(5,3);
-asd(2)=2.*asd(1);
+[asa(1),asda(1),asd2a(1)]=finaltextfirsta(5,3);
+asda(2)=2.*asda(1);
 1
-while(asd(2)>asd(1));
+while(asda(2)>asda(1));
     
-    [as(2),asd(2),asd2(2)]=finaltextfirsta(5,5);
+    [asa(2),asda(2),asd2a(2)]=finaltextfirsta(5,5);
     
 end
 2
-asd(3)=2.*asd(2);
-while(asd(3)>asd(2));
-    [as(3),asd(3),asd2(3)]=finaltextfirsta(5,7);
+asda(3)=2.*asda(2);
+while(asda(3)>asda(2));
+    [asa(3),asda(3),asd2a(3)]=finaltextfirsta(5,7);
     
 end
 3
-asd(4)=2.*asd(3);
-while(asd(4)>asd(3));
+asda(4)=2.*asda(3);
+while(asda(4)>asda(3));
      
-    [as(4),asd(4),asd2(4)]=finaltextfirsta(5,9);
+    [asa(4),asda(4),asd2a(4)]=finaltextfirsta(5,9);
 end
 
-as
-asd2
+asa
+asd2a
